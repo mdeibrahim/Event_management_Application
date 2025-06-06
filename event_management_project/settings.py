634d910967b69df506e47a301bdd4fe59b0ccfc4
mdeibrahim <mmeibrahim505@gmail.com>
2025-06-06 17:47:15 +0600

    update settings

--- event_management_project/settings.py
+++ event_management_project/settings.py
@@ -137,12 +137,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'theme/static'),
     BASE_DIR / "static",
     # BASE_DIR / "theme" / "static",
     BASE_DIR / "theme" / "static_src",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = '/media/'
