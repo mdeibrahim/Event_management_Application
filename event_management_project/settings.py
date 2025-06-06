31dd3a17a8ece1c4038eb3c08fdfa264283b07d6
mdeibrahim <mmeibrahim505@gmail.com>
2025-06-06 17:02:39 +0600

    back to commit21

--- event_management_project/settings.py
+++ event_management_project/settings.py
@@ -6,15 +6,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-@^a^w_j6wwedbqtq2ciat6zf#022m^_fedd)0&2s!(duj*-u(y')
 SECRET_KEY =config('SECRET_KEY')
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG', 'True') == 'True'
@@ -24,7 +16,6 @@ CSRF_TRUSTED_ORIGIN= ['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
